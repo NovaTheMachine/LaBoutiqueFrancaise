d88532765c73dc095b6e32cd9744d519614164b8
NovaTheMachine <nfdistri@gmail.com>
2021-08-19 13:25:36 +0200

    invalid message

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,23 +20,34 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname',TextType::class,[
                 'label'=> 'Prénom',
-                'constraints' => [new Length( ['min'=> 2 ,'max'=> 20 ] ) ],
+                'constraints' => [new Length( ['min'=> 3 ,'max'=> 20 ,
+                'minMessage' => 'Votre prénom doit avoir au moins  {{ limit }} characteres',
+                'maxMessage' => 'Votre prénom doit avoir au moins {{ limit }} caracteres',] ) ],
                 'attr'=>['placeholder'=>'Saisissez votre prénom']])
             ->add('lastname',TextType::class,
                 ['label'=> 'Nom',
-                'constraints' => [new Length( ['min'=> 2 ,'max'=> 20 ] ) ],
+                'constraints' => [new Length( ['min'=> 3 ,'max'=> 20 ,
+                'minMessage' => 'Votre nom doit avoir au moins  {{ limit }} characteres',
+                'maxMessage' => 'Votre nom doit avoir au moins {{ limit }} caracteres', ] ) ],
                 'attr'=>['placeholder'=>'Saisissez votre nom']])
             ->add('email',EmailType::class,
                 ['label'=> 'E-mail',
                 'attr'=>['placeholder'=>'Saisissez votre adresse mail']])
-            ->add('password',PasswordType::class,
-                ['label'=> 'Mot de passe',
-                'constraints' => [new Length( ['min'=> 2 ,'max'=> 20 ] ) ],
-                'attr'=>['placeholder'=>'Creez votre mot de passe']])
-            ->add('passwordConfirm',PasswordType::class,
-                ['label'=> 'Confirmez',
-                'mapped' => false,
-                'attr'=>['placeholder'=>'Confirmez votre mot de passe']])
+            ->add('password',RepeatedType::class,
+                ['type'=> PasswordType::class,
+                'invalid_message'=>'les mots de passe doivent etre identiques',
+                    'label'=> 'Mot de passe',
+                    'required'=>'true',
+                    'first_options' => [
+                        'label' => 'Mot de passe',
+                        'attr'=>['placeholder'=>'Saisissez votre mot de passe']],
+                        'second_options' => [
+                            'label' => 'Mot de passe',
+                            'attr'=>['placeholder'=>'Confirmez votre mot de passe']]])
+                        
+
+                
+                
                    
             ->add('submit',SubmitType::class,['label'=>"S'inscrire"])
             ;
